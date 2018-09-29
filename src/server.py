af983fa8f40a198b0dd48e6a23e819129cfa75ee
Dheeraj Bhadani <dheeraj.bhadani@gmail.com>
2018-09-29 21:18:50 +0000

    docstrings added.

--- src/server.py
+++ src/server.py
@@ -1,3 +1,17 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python gRPC db-sync Server."""
+
 import time
 import grpc
 
@@ -7,6 +21,7 @@ from concurrent import futures
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class DbSyncServicer(db_sync_pb2_grpc.DbSyncServicer):
+    """Implements the dbsync API server."""
     def Copy(self, request, conext):
         with open(request.name, "rb") as file_obj:
             while True:
@@ -19,6 +34,7 @@ class DbSyncServicer(db_sync_pb2_grpc.DbSyncServicer):
 
 
 def serve():
+    """Configures and runs the dbsync API server."""
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     db_sync_pb2_grpc.add_DbSyncServicer_to_server(DbSyncServicer(), server)
     server.add_insecure_port('[::]:40084')
